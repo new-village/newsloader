7370fcd926b47ba808adfa0cbc90a1615fe560eb
new-village <kazuki.niimura@gmail.com>
2023-05-03 01:45:22 +0000

    run test

--- nsloader/test.py
+++ nsloader/test.py
@@ -1,19 +1,26 @@
 """ test.py
 """
 import logging
+ 
+
+
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 import chromedriver_binary
 
 def exec():
     options = Options()
     options.add_argument('--headless')
-    driver = webdriver.Chrome(options=options)
+    options.add_argument("--disable-dev-shm-usage")
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
 
     url = 'https://www.wsj.com/'
     driver.get(url)
     logging.info(f'== Success to %s ==' % url)
-    driver.save_screenshot('screenshot.png')
+    # driver.save_screenshot('screenshot.png')
     driver.close()
     driver.quit()
 
